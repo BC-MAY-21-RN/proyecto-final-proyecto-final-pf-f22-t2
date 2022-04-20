ed1c3d80a2606d382dc0768b505cf2f34184bd6a
Kelly Alzate <kejoh07@gmail.com>
2022-04-20 23:27:35 +0200

    Add safeAreaView for ios version,
    Co-authored-by: Vanina Ayelen Winnik <VaninaWinnik@users.noreply.github.com>
    Co-authored-by: Alberto Gonzalez <w01fgxng@users.noreply.github.com>
    Co-authored-by: Oscar Villarraga  <osvilla27@users.noreply.github.com>

--- TuristicApp/src/screens/SignupScreen/index.js
+++ TuristicApp/src/screens/SignupScreen/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import {View, Text, TextInput} from 'react-native';
 import {ButtonPrimary} from '../../components/ButtonPrimary/';
 import { ButtonSecond } from '../../components/ButtonSecond';
+import { SafeAreaView } from 'react-native';
 
 export const SignupScreen = () => {
   return (
-    <View>
-     <Text>Sign In</Text>
-     <TextInput label={'Email'} placeholder={'example@email.com'}/>
-     <TextInput placeholder={'Password'}/>
-     <ButtonPrimary text={'Sign In'}/>
-     <ButtonSecond text={'Sign In with Google'}/>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text>Sign In</Text>
+        <TextInput label={'Email'} placeholder={'example@email.com'}/>
+        <TextInput placeholder={'Password'}/>
+        <ButtonPrimary text={'Sign In'}/>
+        <ButtonSecond text={'Sign In with Google'}/>
+      </View>
+    </SafeAreaView>
   );
 };
