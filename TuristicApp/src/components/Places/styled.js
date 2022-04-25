48625227a7ba676061c2e0141c81185351c24acf
Vanina <vaniwinnik@gmail.com>
2022-04-25 20:00:28 -0300

    Resolve codebeat conflicts files,
    Co-authored-by: Vanina <vaniwinnik@gmail.com>
    Co-authored-by: Kelly <kejoh07@gmail.com>
    Co-authored-by: Oscar <oscaravg27@gmail.com>
    Co-authored-by: Alberto <jgonzalez48@ucol.mx>

--- TuristicApp/src/components/Places/styled.js
+++ TuristicApp/src/components/Places/styled.js
@@ -4,27 +4,3 @@ export const Container = styled.View`
    paddin: 10px 10px;
    background-color: white;
 `
-export const ContainerPlace = styled.View`
-    height: 180px;
-    padding: 5px;
-    margin: 10px;
-    background-color: #edf6f9;
-    border-radius: 15px;
-    flex-direction: row;
-`
-export const Content = styled.View`
-    height: 100%;
-    width: 50%;
-    padding: 10px;
-    flex: 1;
-    justify-content: space-between;
-`
-export const ImageStyled = styled.Image`
-    height: 100%;
-    width: 100%;
-    border-radius: 15px;
-`
-export const Location = styled.TouchableOpacity`
-    flex-direction: row;
-    align-items: center;
-`
---
+++ TuristicApp/src/components/PlacesContent/styled.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components/native';
+
+export const ContainerPlace = styled.View`
+    height: 180px;
+    padding: 5px;
+    margin: 10px;
+    background-color: #edf6f9;
+    border-radius: 15px;
+    flex-direction: row;
+`
+export const Content = styled.View`
+    height: 100%;
+    width: 50%;
+    padding: 10px;
+    flex: 1;
+    justify-content: space-between;
+`
+export const ImageStyled = styled.Image`
+    height: 100%;
+    width: 100%;
+    border-radius: 15px;
+`
+export const Location = styled.TouchableOpacity`
+    flex-direction: row;
+    align-items: center;
+`
