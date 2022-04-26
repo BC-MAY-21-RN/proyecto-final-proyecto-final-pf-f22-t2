e15579a6a29aa617c39133dd3157e7ab79de37ae
Kelly Alzate <kejoh07@gmail.com>
2022-04-27 01:56:50 +0200

    Add chevron icon and styles,
    Co-authored-by: Alberto Gonzalez <w01fgxng@users.noreply.github.com>
    Co-authored-by: Vanina Ayelen Winnik <VaninaWinnik@users.noreply.github.com>
    Co-authored-by: Oscar Villarraga  <osvilla27@users.noreply.github.com>

--- TuristicApp/src/components/SideMenu/index.js
+++ TuristicApp/src/components/SideMenu/index.js
@@ -1,55 +1,63 @@
-import * as React from 'react';
+import React from 'react';
+import { View, Image } from 'react-native';
 import { Drawer } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Avatar } from 'react-native-paper';
-import styles from '../SideMenu/styles'
+import { styles } from './styles';
 
 const SideMenu = () => {
   const [active, setActive] = React.useState('');
 
 
   return (
-    <SafeAreaView>
-    <Avatar.Image size={54} source={require('../../assets/avatar.png')} />
-    <Drawer.Section title="Johanna Guide">
-      <Drawer.Item
-        icon={"home"}
-        label="Home"
-        active={active === 'one'}
-        onPress={() => setActive('first')}
-      />
-      <Drawer.Item
-        icon={"map"}
-        label="Place"
-        active={active === 'two'}
-        onPress={() => setActive('second')}
-      />
-      <Drawer.Item
-        icon={"calendar"}
-        label="Events"
-        active={active === 'three'}
-        onPress={() => setActive('second')}
-      />
-      <Drawer.Item
-        icon={"apple"}
-        label="Food"
-        active={active === 'four'}
-        onPress={() => setActive('second')}
-      />
-      <Drawer.Item
-        icon={"heart"}
-        label="Rate"
-        active={active === 'five'}
-        onPress={() => setActive('second')}
-      />
-      <Drawer.Item
-        icon={"logout"}
-        label="Logout"
-        active={active === 'six'}
-        onPress={() => setActive('second')}
-      />
-    </Drawer.Section>
-    </SafeAreaView>
+    <View style={styles.sidemenu}>
+      <SafeAreaView>
+        <View>
+          <Avatar.Image size={54} source={require('../../assets/avatar.png')} />
+            <Drawer.Section title="Johanna Guide">
+              <View>
+              <Drawer.Item
+                icon={"home"}
+                label="Home"
+                active={active === 'one'}
+                onPress={() => setActive('first')}
+              />
+              <Avatar.Icon size={40} icon="chevron-right" backgroundColor="transparent" color="black" />
+              </View>
+              <Drawer.Item
+                icon={"map"}
+                label="Place"
+                active={active === 'two'}
+                onPress={() => setActive('second')}
+              />
+              <Drawer.Item
+                icon={"calendar"}
+                label="Events"
+                active={active === 'three'}
+                onPress={() => setActive('second')}
+              />
+              <Drawer.Item
+                icon={"food"}
+                label="Food"
+                active={active === 'four'}
+                onPress={() => setActive('second')}
+              />
+              <Drawer.Item
+                icon={"heart"}
+                label="Rate"
+                active={active === 'five'}
+                onPress={() => setActive('second')}
+              />
+              <Drawer.Item
+                icon={"logout"}
+                label="Logout"
+                active={active === 'six'}
+                onPress={() => setActive('second')}
+              />
+            </Drawer.Section>
+        </View>
+      </SafeAreaView>
+    </View>
   );
 };
 
--- TuristicApp/src/components/SideMenu/styles.js
+++ TuristicApp/src/components/SideMenu/styles.js
@@ -1,10 +1,13 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-    input: {
-      height: 40,
-      margin: 12,
-      borderBottomWidth: 3,
-      padding: 10,
+    sidemenu: {
+      width: "55%",
+      height: "100%",
+      backgroundColor: "#f75f6a",
+      paddingTop: 50,
+    },
+    homeIcon: {
+        flex: 1,
     },
 })
