20dd87428ac8db7e07309362dc91f3472b60ec0a
Kelly Alzate <kejoh07@gmail.com>
2022-04-30 00:49:35 +0200

    Solve master errors

--- TuristicApp/src/navigation/Navigation.js
+++ TuristicApp/src/navigation/Navigation.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {infoUser} from '../library/utils/auth';
-import {SignupScreen} from '../screens/SignupScreen/index';
-import {HomeScreen} from '../screens/HomeScreen/index';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { infoUser } from '../library/utils/auth';
+import { SignupScreen } from '../screens/SignupScreen/index';
+import { HomeScreen } from '../screens/HomeScreen/index';
 
 
 const Stack = createNativeStackNavigator();
--- TuristicApp/src/screens/HomeScreen/index.js
+++ TuristicApp/src/screens/HomeScreen/index.js
@@ -1,25 +1,15 @@
-import { View } from 'react-native'
-import React from 'react'
-import Icon from 'react-native-vector-icons/FontAwesome5';
-import { signOut } from '../../library/utils/auth';
+import { View } from 'react-native';
+import React from 'react';
 import { ImageCarousel } from '../../components/ImageCarousel';
-import { MyText } from '../../components/MyText';
 import { Places } from '../../components/Places';
-import HeaderBar from '../../components/Header-bar/index';
+import {HeaderBar} from '../../components/Header-bar';
 
 
-export const HomeScreen = ({navigation, props}) => {
-  return (
-    
+export const HomeScreen = ({navigation}) => {
+return (
     <View>
-      <MyText color={'#607d8b'} size={'30px'} bold>{'HOME'}</MyText>
-      <Icon name="power-off" 
-        size={25}
-        color={'#4e4d57'}
-        onPress={signOut} />
-      <ImageCarousel />
-      
-      <HeaderBar title={"Home"} color={"white"}  style={{ backgroundColor: '#607d8b' }} />
+      <HeaderBar title={"Places"} color={"white"} style={{ backgroundColor: '#607d8b' }} />
+      {/* <ImageCarousel/> */}
       <Places/>
     </View>
   );
