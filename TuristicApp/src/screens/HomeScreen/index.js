8314afb5ca6611270c785172dc082d85429356b8
Kelly Alzate <kejoh07@gmail.com>
2022-05-11 23:49:59 +0200

    Add API maps and create map components,
    Co-authored-by: Vanina <vaniwinnik@gmail.com>
    Co-authored-by: Kelly <kejoh07@gmail.com>
    Co-authored-by: Oscar <oscaravg27@gmail.com>
    Co-authored-by: Alberto <jgonzalez48@ucol.mx>

--- TuristicApp/src/screens/HomeScreen/index.js
+++ TuristicApp/src/screens/HomeScreen/index.js
@@ -2,12 +2,14 @@ import { View } from 'react-native'
 import React from 'react'
 {/*import { ImageCarousel } from '../../components/ImageCarousel/index';*/}
 import HeaderBar from '../../components/Header-bar/index';
+import { Maps } from '../../components/Maps/index';
 
 export const HomeScreen = ({navigation, props}) => {
   return (
     <View>
       <HeaderBar title={"Home"} color={"white"}  style={{ backgroundColor: '#607d8b' }} />
       {/*<ImageCarousel />*/}
+      <Maps />
     </View>
   );
 };
