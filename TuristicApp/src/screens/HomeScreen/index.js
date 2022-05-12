427b51206fc6d55caeccd29ff55f24e8048857fc
osvilla27 <oscaravg27@gmail.com>
2022-05-12 11:03:46 -0500

    Backend validations

--- TuristicApp/src/screens/HomeScreen/index.js
+++ TuristicApp/src/screens/HomeScreen/index.js
@@ -1,13 +1,21 @@
-import { View } from 'react-native'
+import { View, Pressable, Text } from 'react-native'
 import React from 'react'
 {/*import { ImageCarousel } from '../../components/ImageCarousel/index';*/}
 import HeaderBar from '../../components/Header-bar/index';
+import { signOut } from '../../library/utils/auth';
+
+
 
 export const HomeScreen = ({navigation, props}) => {
   return (
     <View>
       <HeaderBar title={"Home"} color={"white"}  style={{ backgroundColor: '#607d8b' }} />
       {/*<ImageCarousel />*/}
+
+      <Pressable onPress={signOut}>
+        <Text>Salir de sesión</Text>
+      </Pressable>
     </View>
+
   );
 };
--- TuristicApp/src/screens/SignupScreen/index.js
+++ TuristicApp/src/screens/SignupScreen/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import { Button } from '../../components/MyButton';
 import { Logo } from '../../components/Logo';
 import { MyText } from '../../components/MyText';
@@ -9,22 +9,29 @@ import {
 } from '../../library/utils/styledGlobal'
 import { Check } from '../../components/Check';
 import { Footer } from '../../components/Footer';
-import { signIn, signUp } from '../../library/utils/auth'
 import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { AuthContext } from '../../library/utils/auth'
 
 export const SignupScreen = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [terms, setTerms] = useState(false)
   const [isLoginScreen, setIsLoginScreen] = useState(false)
+
+  const {signUp, signIn, errorEmail, errorPassword, setErrorEmail} = useContext(AuthContext)
   
   const handleSingUp = () =>{
+    setErrorEmail(null)
     signUp(email, password);
   }
 
   const handleLogin = () => {
+    setErrorEmail(null)
     if (email != '' && password != '') {
+      setErrorEmail(null)
       signIn(email, password);
+      
     }
   }
 
@@ -37,19 +44,21 @@ export const SignupScreen = () => {
         <MyText color={'#4e4d57'} size={'30px'} bold>{isLoginScreen?'Sign In':'Sign Up'}</MyText>
         {isLoginScreen&&<MyText bold>Welcome to Huila</MyText>}
         <MyInput text={'Email'} placeholder={'Your email address'}
+          error={errorEmail}
           onChangeText={value => setEmail(value)}
           value={email} disabled={email?true:false}
         />
         <MyInput text={'Password'}
+          error={errorPassword}
           onChangeText={value => setPassword(value)}
           value={password} disabled={password?true:false}
           secureTextEntry={true}
         />
         {!isLoginScreen&&<Check value={terms}  onValueChange={value => setTerms(value)} />} 
       </ContainerLeft>
-      <Button onPress={isLoginScreen?handleLogin:handleSingUp} text={'Sign In'}/>
+      <Button onPress={isLoginScreen?handleLogin:handleSingUp} text={isLoginScreen?'Sign In':'Sign Up'}/>
       <MyText bold size={'14px'}>Or</MyText>
-      <Button icon text={'Sign In with Google'}/>
+      <Button icon text={isLoginScreen?'Sign In with Google':'Sign Up with Google'}/>
       <Footer isLoginScreen={isLoginScreen} onPress={() => setIsLoginScreen(!isLoginScreen)}/>
     </ContainerCenter>
   );
