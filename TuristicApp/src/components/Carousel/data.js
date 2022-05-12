86d353e60914bd6dc8d56eca2a5cc883901a894b
Vanina <vaniwinnik@gmail.com>
2022-05-12 16:45:45 -0300

    Refactor Carousel component

---
+++ TuristicApp/src/components/Carousel/data.js
@@ -0,0 +1,14 @@
+export default data = [
+  {
+    title: "Aenean leo",
+    imgUrl: "https://i.pinimg.com/736x/21/43/7f/21437f0bd7a33ce7bc5e7c4ec33c7fea.jpg"
+  },
+  {
+    title: "In turpis",
+    imgUrl: "https://huilamagnifica.com/wp-content/uploads/2019/05/Mirador-La-Mano-del-Gigante-Municipio-de-Gigante-Huila-Magnifica-01.jpg"
+  },
+  {
+    title: "Lorem Ipsum",
+    imgUrl: "https://pbs.twimg.com/media/EwP59FTXAAQMEwk.jpg"
+  }
+]
