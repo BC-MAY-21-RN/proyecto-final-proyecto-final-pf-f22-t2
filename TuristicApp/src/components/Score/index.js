4d14d7439cc1c98305c65703dd5dd2b689bbd8e5
Vanina <vaniwinnik@gmail.com>
2022-05-25 18:32:21 -0300

    Fixes in necessary props

--- TuristicApp/src/components/Score/index.js
+++ TuristicApp/src/components/Score/index.js
@@ -3,12 +3,15 @@ import React from 'react'
 import { Container } from './styled'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { MyText } from '../MyText';
+import { updateRating } from '../../services/addPlaces'
 
-export const Score = ({likes, dislikes}) => {
+export const Score = ({ item }) => {
+  const likes = item.place.rating
+  const dislikes = (item.place.ratingTotal - item.place.rating)
   return (
     <Container>
       <TouchableOpacity>
-        <Icon name="cards-heart" size={30} color="#f75f6a"/>
+        <Icon name="cards-heart" size={30} color="#f75f6a" onPress={() => updateRating(item)}/>
       </TouchableOpacity>
       <MyText bold color={'black'}>{likes}</MyText>
       <TouchableOpacity>
